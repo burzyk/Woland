c10ec7337fa42c3c3ca8aa33548d2b9af0e04669
burzyk <p.k.burzynski@gmail.com>
2016-06-21 22:09:11 +0100

    VS projects

---
+++ src/Woland.Service/Domain/Entities/JobLead.cs
@@ -0,0 +1,12 @@
+//-----------------------------------------------------------------------
+// <copyright file="IServiceLog.cs" company="burzyk">
+//     Copyright (c) burzyk. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Woland.Service.Domain.Entities
+{
+    public class JobLead
+    {
+
+    }
+}
